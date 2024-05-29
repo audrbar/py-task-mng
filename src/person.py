beec56db18475b2a862d488a62a04508122af6d7
audrbar <audrbar@gmail.com>
2024-05-29 20:55:52 +0300

    Add: person.py

---
+++ src/person.py
@@ -0,0 +1,16 @@
+from organization import Organization
+
+
+class Person:
+    def __init__(self, person_id: str, person_name: str, person_surname: str, person_email: str, person_phone: str,
+                 organization: Organization):
+        self.person_id = person_id
+        self.person_name = person_name
+        self.person_surname = person_surname
+        self.person_email = person_email
+        self.person_phone = person_phone
+        self.organization = organization
+
+    def get_org_details(self):
+        return (f"Person {self.person_id}, {self.person_name}, {self.person_surname}, {self.person_email}, "
+                f"{self.person_phone}")
